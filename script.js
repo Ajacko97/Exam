f24440f0698dc8094d647ca7df99c50afb46e6ce
Ajacko97 <45338619+Ajacko97@users.noreply.github.com>
2019-05-22 01:38:14 +0200

    Changes

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ $(document).ready(function () {
 		}, 1250);
 	});
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+	} else {
+		document.getElementById("myBtn").style.display = "none";
+	}
+}
+//When you clicked arrowup you going to top of the website.
+$(document).ready(function () {
+	$("#myBtn").click(function () {
+		$('html, body').animate({
+			scrollTop: $(".container").offset().top
+		}, 1000);
+	});
+});
